26138d8ae8e672776888b1a56c2abce3103edaae
ganinw13120 <gan131201@gmail.com>
2024-01-10 14:57:46 +0700

    [.] initial project

---
+++ mongo.go
@@ -0,0 +1,28 @@
+package xk6_mongo
+
+import (
+	"fmt"
+	k6modules "go.k6.io/k6/js/modules"
+)
+
+// Register the extension on module initialization, available to
+// import from JS as "k6/x/mongo".
+func init() {
+	k6modules.Register("k6/x/mongo", new(Mongo))
+}
+
+// Mongo is the k6 extension for a Mongo client.
+type Mongo struct{}
+
+// Client is the Mongo client wrapper.
+type Client struct {
+}
+
+// NewClient represents the Client constructor (i.e. `new mongo.Client()`) and
+// returns a new Mongo client object.
+// connURI -> mongodb://username:password@address:port/db?connect=direct
+func (*Mongo) NewClient(connURI string) interface{} {
+	fmt.Println("asd")
+
+	return &Client{}
+}
