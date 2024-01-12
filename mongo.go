77f791ee9b4f42dba6002cbd4b6f5198bfa20f09
ganinw13120 <gan131201@gmail.com>
2024-01-12 14:56:05 +0700

    [.] fix lint

--- mongo.go
+++ mongo.go
@@ -3,10 +3,8 @@ package xk6_mongo
 import (
 	"context"
 	"fmt"
-	"github.com/ganinw13120/xk6-mongo/database"
 	"github.com/ganinw13120/xk6-mongo/mongo"
 	k6modules "go.k6.io/k6/js/modules"
-	"go.mongodb.org/mongo-driver/bson"
 )
 
 func init() {
@@ -14,24 +12,6 @@ func init() {
 }
 
 type Mongo struct{}
-
-func main() {
-	client, err := database.NewMongoDBConnection(context.TODO(), "mongodb+srv://spider-data-history:wYxoSKhlt6Np7060@spider-dev.673kr.mongodb.net/")
-	if err != nil {
-		panic(err)
-	}
-	db := database.NewMongoDB(client)
-	fmt.Println("Connected")
-	col := client.Database("history").Collection("history-account")
-	var result interface{}
-
-	err = db.FindOne(context.TODO(), col, &result, bson.E{"id", ""})
-	fmt.Println(result)
-	fmt.Println(err)
-	//as := Mongo{}
-	//as.NewClient("mongodb://gan:lHxWqJM30n3gthny@spider-dev.673kr.mongodb.net", "history", "history-post", "")
-	//as.NewClient("mongodb+srv://spider-data-history:wYxoSKhlt6Np7060@spider-dev.673kr.mongodb.net/", "history", "history-post", "")
-}
 
 func (*Mongo) NewClient(uri, database, collection string, pipeline interface{}) interface{} {
 	client, err := mongo.NewMongoDBConnection(context.TODO(), uri)
