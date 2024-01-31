55bd6ab933f41ea5048bc76cc1c403a22ee81fef
ganinw13120 <gan131201@gmail.com>
2024-01-31 14:21:32 +0700

    [.] Update makefile and re-build

--- test.js
+++ test.js
@@ -1,29 +1,36 @@
 import xk6_mongo from 'k6/x/mongo';
 
-const client = xk6_mongo.newClient('mongodb://localhost:27017/', "db","collection");
+const client = xk6_mongo.newClient('mongodb://localhost:27017/', "db", "collection");
 
 export const options = {
     vus: 10,
     duration: '5m',
 }
 
-export default ()=> {
+export default () => {
     const result = client.aggregate([
         {
             $match: {
-                id: "",
+                name: "John",
             },
         },
         {
             $project: {
-                meta: 1,
-                collected_at: 1
+                name: 1,
+                address: 1,
+                created_at: 1
             },
         },
         {
             $sort: {
-                collected_at: -1,
+                created_at: -1,
             },
         },
     ])
+
+    console.log(result)
+
+    check(result, {
+        'Has result': (r) => r.length > 0,
+    });
 }
